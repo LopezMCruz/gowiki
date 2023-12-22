9e511cc6830bfb35c93b0302880fe691a9c14450
Cruz <cml231@txstate.edu>
2023-12-22 15:25:03 -0600

    finished validation

--- wiki.go
+++ wiki.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"net/http"
 	"log"
+	"errors"
 )
 
 // define page as a struct with two fields representing the tile and body
@@ -45,7 +46,10 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request){
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w,r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil{
 		http.Redirect( w, r, "/edit/"+title,http.StatusFound)
@@ -56,7 +60,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request){
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request){
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w,r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -65,15 +72,28 @@ func editHandler(w http.ResponseWriter, r *http.Request){
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request){
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w,r)
+	if err != nil {
+		return
+	}
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	err = p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request)(string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w,r)
+		return "", errors.New("invalid Page Title")
+	}
+
+	return m[2], nil // The title is the second subexpression
 }
 
 func main() {
